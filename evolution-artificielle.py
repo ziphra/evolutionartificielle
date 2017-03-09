c0a5534a35975e30cb274187790630ad2c0d1f36
ziphra <s.euphrasie@yahoo.fr>
2017-03-10 00:28:07 +0100

    Ajout de la fonction mutation

--- evolution-artificielle.py
+++ evolution-artificielle.py
@@ -1,45 +1,41 @@
 __author__ = 'euphrasieservant'
 # population de départ : N séquences d'ARNt de longueur l =
-# chaques séquences est constituées de l nucléotides choisis au hasard
+# chaque séquence est constitué de l nucléotides choisis au hasard
 import random
 
 
 # constitution de la population de départ
-def PopDeDepart(l):
+def PopDeDepart(l,N):
 
-    atgc = ["A","U","G", "C"]
+    augc = ["A","U","G", "C"]
     sequences = []
 
-    for i in range(10000):
+    for i in range(N):
         seq = []
         for y in range(l):
             tirage = random.randint(0, 3)
-            nuc = atgc[tirage]
+            nuc = augc[tirage]
             seq.append(nuc)
         sequences.append(seq)
     return sequences
 
 
-# phénotype
-# appariements de la séquence cible, sous forme d'une liste de paires de positions
-# fonctions qui génère la liste des appariements
+# PHÉNOTYPE
+# fonction qui génère la liste des appariements
+# sous forme d'une liste de paires de positions
 def ListeAppariements(repli):
     listeapp = []
-    for i in range(0, 4):
-        pos = []
-        a = y = repli[i][0]
-        b = repli[i][2]
-        z = repli[i][1]
-        pos.append(a)
-        pos.append(b)
-        listeapp.append(pos)
-        for j in range(y, z):
+    for i in range(0, len(repli)):
+        a = repli[i][0]  #premier élément du repli dans le sens 5'-3'   (1 dans le cas du repli1)
+        b = repli[i][2]  #premier élement du repli dans le sens 3'-5'   (72 dans le cas du repli1)
+        c = repli[i][1]  #dernier element du repli dans le sens 5'-3'   (7 dans le cas du repli1)
+        for j in range(a, c+1):
             pos = []
-            a += 1
-            b -= 1
             pos.append(a)
             pos.append(b)
             listeapp.append(pos)
+            a += 1
+            b -= 1
     return listeapp
 
 
@@ -65,18 +61,18 @@ def score(listeapp, seq):
     return score
 
 #reproduction
-#le score est proportionelle à la probabilité de se reproduire
+#le score est proportionel à la probabilité de se reproduire
 #score de 5 = 5/21 chances de se reproduire (car 21 = nombre d'appariements de la cible)
 
 def reproduction(ListeScore, sequences):
-    c = 0
+    c = 0  #un compteur qui nous indique combien de nouvelles séquences ont été crées
     for i in ListeScore:
         scorei = ListeScore[i]
         n = random.randint(0,20)
         if n <= scorei:
             NewSeq = sequences[i]
+            mutation(NewSeq,probamutation)
             sequences.append(NewSeq)
-            print(NewSeq)
             c+=1
     print(c)
 
@@ -84,12 +80,44 @@ def reproduction(ListeScore, sequences):
 
 
 
-#P R O G R A M M E P R I N C I P A L E
+# param reps: une liste de Repli
+def ListeAppariements_NEW(reps):
+    listeapp = []
+    for i in range(0, len(reps)):  #parce que il y a 4 repliements dans la séquence cible
+        a = reps[i]._a  #premier élément du repli dans le sens 5'-3'   (1 dans le cas du repli1)
+        b = reps[i]._b  #premier élement du repli dans le sens 3'-5'   (72 dans le cas du repli1)
+        c = reps[i]._c  #dernier element du repli dans le sens 5'-3'   (7 dans le cas du repli1)
+        for j in range(a, b+1):
+            pos = []
+            pos.append(a)
+            pos.append(c)
+            listeapp.append(pos)
+            a += 1
+            c -= 1
+    return listeapp
+
+# induit une mutation avec une probabilité de 1/100
+def mutation(seq,probamutation):  #seq = un seul ARNt
+    tirage = random.randint(1,100)
+    augc = ["A","U","G", "C"]
+    if tirage <= probamutation: #  1 chance sur 100
+        pos_mutation = random.randint(0,(len(seq)-1))  # une position choisie au hasard dans la séquence de longueur l
+        nuc = augc[random.randint(0,3)]  # un nucléotide de remplacement choisi au hasard
+        while nuc == seq[pos_mutation]:  # au cas ou le nucléotide de remplacement est le même que l'original
+            nuc = augc[random.randint(0,3)]
+        else:
+            seq[pos_mutation] = nuc
+            print(seq)
+    return seq
+
+
+
+#P R O G R A M M E  P R I N C I P A L
 
 #population de depart
 l = 75  # longueur de la séquence
-
-sequences = PopDeDepart(l)
+N = 10000  #nombre de chaines
+sequences = PopDeDepart(l,N)
 
 #appariements
 repli1 = [1, 7, 72]
@@ -99,6 +127,7 @@ repli4 = [49, 53, 65]
 repli = [repli1] + [repli2] + [repli3] + [repli4]
 
 listeapp = (ListeAppariements(repli))
+print(listeapp)
 
 #score
 # range les scores dans une liste
@@ -109,8 +138,14 @@ for i in range(len(sequences)):
     ListeScore.append(ScoreSeq)
 print(ListeScore)
 
+# mutation
+probamutation = 100
+
 #reproduction
 #Après avoir compté le nombre de nouvelles séquences créées après la première genération, et l'avoir comparé avec les scores,
 # le resultat de cette fonction semble cohérent.
-# À revérifier : test plus précis
+# => Les scores oscillent autour de 5. Si en moyenne, une séquence a un score de 5 (sur un maximum de 21)
+# la probabilité de se reproduire est de 6/21
+# (6/21) * 10 000 = 2 857 => à peu près égale aux nombres de nouvelles séquences créées
+
 reproduction(ListeScore, sequences)
